5518c3d87bf7111e5a0482ba422d2df549392b5e
Adria Blancafort <adriablancafort@gmail.com>
2024-11-20 23:44:09 +0100

    deactivate wordpress default scripts

--- theme/functions.php
+++ theme/functions.php
@@ -195,3 +195,72 @@ require get_template_directory() . '/inc/template-tags.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
+
+
+
+
+
+/**
+ * Disable emojis.
+ *
+ * @return void
+ */
+function disable_wp_emojicons()
+{
+	// all actions related to emojis.
+	remove_action('admin_print_styles', 'print_emoji_styles');
+	remove_action('wp_head', 'print_emoji_detection_script', 7);
+	remove_action('admin_print_scripts', 'print_emoji_detection_script');
+	remove_action('wp_print_styles', 'print_emoji_styles');
+	remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+	remove_filter('the_content_feed', 'wp_staticize_emoji');
+	remove_filter('comment_text_rss', 'wp_staticize_emoji');
+
+	// filter to remove TinyMCE emojis.
+	add_filter('tiny_mce_plugins', 'disable_emojicons_tinymce');
+}
+add_action('init', 'disable_wp_emojicons');
+
+/**
+ * Disable in TinyMCE editor.
+ *
+ * @param array $plugins tinymce plugins.
+ * @return array
+ */
+function disable_emojicons_tinymce($plugins)
+{
+	if (is_array($plugins)) {
+		return array_diff($plugins, array('wpemoji'));
+	} else {
+		return array();
+	}
+}
+
+/**
+ * Disable embeds
+ *
+ * @return void
+ */
+function deregister_footer_scripts()
+{
+	wp_dequeue_script('wp-embed');
+}
+add_action('wp_footer', 'deregister_footer_scripts');
+
+
+// Remove Gutenberg Block Library CSS from loading on the frontend
+function remove_wp_block_library_css()
+{
+	wp_dequeue_style('wp-block-library');
+	wp_dequeue_style('wp-block-library-theme');
+	wp_dequeue_style('wc-blocks-style'); // Remove WooCommerce block CSS
+}
+add_action('wp_enqueue_scripts', 'remove_wp_block_library_css', 100);
+
+
+// Remove Global Styles Inline CSS
+function remove_global_styles()
+{
+	wp_dequeue_style('global-styles');
+}
+add_action('wp_enqueue_scripts', 'remove_global_styles', 100);
